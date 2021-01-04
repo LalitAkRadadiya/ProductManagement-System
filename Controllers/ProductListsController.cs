27489f4308c5759275dc80a5e08ecff1037cf790
Lalit Radadiya <radadiya7lalit7@gmail.com>
2021-01-04 23:06:02 +0530

    init

---
+++ Controllers/ProductListsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProductManagment;
+
+namespace ProductManagment.Controllers
+{
+    [Authorize]
+    public class ProductListsController : Controller
+    {
+        private ProductDBEntities db = new ProductDBEntities();
+
+        // GET: ProductLists
+        public ActionResult Index()
+        {
+            return View(db.ProductLists.ToList());
+        }
+
+        // GET: ProductLists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductList productList = db.ProductLists.Find(id);
+            if (productList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productList);
+        }
+
+        // GET: ProductLists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductLists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Category,Price,Quantity,Short_Description,Long_Description,Small__image,Large_image")] ProductList productList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductLists.Add(productList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productList);
+        }
+
+        // GET: ProductLists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductList productList = db.ProductLists.Find(id);
+            if (productList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productList);
+        }
+
+        // POST: ProductLists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Category,Price,Quantity,Short_Description,Long_Description,Small__image,Large_image")] ProductList productList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productList);
+        }
+
+        // GET: ProductLists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductList productList = db.ProductLists.Find(id);
+            if (productList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productList);
+        }
+
+        // POST: ProductLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductList productList = db.ProductLists.Find(id);
+            db.ProductLists.Remove(productList);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
